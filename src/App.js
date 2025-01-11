24a7021d7d60086abb6d8d1b8f1994cfbf51f32e
AUNT1oN256 <anton.mironets@gmail.com>
2025-01-11 16:37:44 +0200

    add sql and table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
-  return <div></div>
+
+  const [color, setColor] = useState('#fff');
+
+  const generateColor = () => {
+    const randomColor = `#${Math.floor(Math.random() * 16999999).toString(16)}`;
+    setColor(randomColor);
+  }
+
+
+  return(
+   <div style={{...styles.container, backgroundColor: color}}>
+  <h1>Color Generator</h1>
+  <p>This color: {color}</p>
+  <button style={styles.button} onClick={generateColor}>Generator color</button>
+  <div></div>
+   </div>
+  );
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    marginTop: '50px',
+    transition: 'background-color 0.5s ease',
+    height: '100vh'
+  },
+  heading: {
+    color: '#007bff'
+  },
+  button: {
+    padding: '10px 20px',
+    margin: '5px',
+    fontSize: '16px',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    background: '#007bff',
+    color: '#fff',
+  }
 }
 
 export default App;
